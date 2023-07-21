cdb0cc0779786b35f590ecd7d1113855499410e9
arc-research-analytics <wwright@atlantaregional.org>
2023-07-21 11:55:34 -0400

    added text animation and Jackson snapshot

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const introParagraph = document.getElementById('intro-paragraph');
+
+    // Hide the paragraph initially
+    introParagraph.style.display = 'none';
+
+    // Delayed display after 1000 milliseconds (1 second)
+    setTimeout(function () {
+        introParagraph.style.opacity = 1;
+        introParagraph.style.display = 'inline'; // Or 'inline', 'inline-block', etc. depending on your layout
+    }, 2700);
+});
